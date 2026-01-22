7b6ed4116717ebf9c1f7aedbaa92302ebe51882f
Nethmi Thambawita <nethmi.thambawita@dmselectronics.com>
2026-01-22 16:42:10 +0530

    remove unwanted files

--- birdmodule/src/config.tsx
+++
@@ -1,9 +0,0 @@
-// export const API_URL = 'http://172.162.242.252:5001';
-// export const API_URL = 'http://20.54.85.178:5001';
-export const API_URL = 'http://82.180.155.215:5001';
-// export const API_URL = 'http://172.20.8.45:5001';
-
-export const API_KEY = '3a034590502b4b4aabf104931243107';
-
-
-
